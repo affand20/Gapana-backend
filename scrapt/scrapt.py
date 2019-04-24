333a3acf8f4f29df66017c0d58849a3f8bfb001e
nazhier9974 <nazhier9974@gmail.com>
2019-04-24 22:01:55 +0700

    Inital Scrapper News

--- scrapt/scrapt.py
+++ scrapt/scrapt.py
@@ -1,8 +1,21 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import selenium
+import requests
+from selenium.webdriver.chrome.options import Options
+from models import models
 
 
-class Scrapt():
+def scrapt_selenium(url):
+    option = Options()
+    option.add_argument('--headless')
+    driver = webdriver.Chrome(options=option)
+    driver.implicitly_wait(20)
+    driver.get(url)
+    bs = BeautifulSoup(driver.page_source, 'lxml')
+    return bs
 
-    def __init__(self):
-        pass
+
+def scrap_requets(url):
+    data = requests.get(url).text
+    return BeautifulSoup(data, 'lxml')
