3665b5ae957a960ab7986a6c39014bfb00d1f76c
nazhier9974 <nazhier9974@gmail.com>
2019-04-24 21:25:45 +0700

    database url

--- models/models.py
+++ models/models.py
@@ -2,7 +2,37 @@ import firebase_admin
 from firebase_admin import db
 
 firebase_admin.initialize_app(options={
-    'databaseURL': 'https://gapana-3283b.firebaseio.com'
+    'apiKey': "AIzaSyAADa1e4P9WSU8xXcidzOaqrqN5DTrbJ4Q",
+    'authDomain': "gapana-3283b.firebaseapp.com",
+    'databaseURL': "https://gapana-3283b.firebaseio.com",
+    'projectId': "gapana-3283b",
+    'storageBucket': "gapana-3283b.appspot.com",
+    'messagingSenderId': "407432302370",
 })
 NEWS = db.reference('news')
 USER = db.reference('user')
+
+
+def _ensure_hero(ids):
+    hero = NEWS.child(ids).get()
+    if not hero:
+        return
+    return hero
+
+
+def viewnews():
+    return NEWS.get()
+
+
+def addnews(data):
+    return NEWS.push(data)
+
+
+def updatenews(ids, data):
+    _ensure_hero(ids)
+    NEWS.child(ids).update(data)
+
+
+def deletenews(ids):
+    _ensure_hero(ids)
+    NEWS.child(ids).delete()
