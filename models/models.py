f469b9458adbf3cecb7037faacf355a400a44091
nazhier9974 <nazhier9974@gmail.com>
2019-04-24 20:56:40 +0700

    database url

--- models/models.py
+++ models/models.py
@@ -2,6 +2,6 @@ import firebase_admin
 from firebase_admin import db
 
 firebase_admin.initialize_app(options={
-    'databaseURL': 'https://<DB_NAME>.firebaseio.com'
+    'databaseURL': 'https://gapana-3283b.firebaseio.com'
 })
-SUPERHEROES = db.reference('superheroes')
+# SUPERHEROES = db.reference('superheroes')
